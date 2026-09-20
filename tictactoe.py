39b2aacff733de2248da41a0246c4c7ecb35fc04
Swarup Panda <swarupanda2006@gmail.com>
2026-09-20 17:53:40 +0530

    Updated tictactoe.py
    Added a few comments to show which concept I implemented.

--- tictactoe.py
+++ tictactoe.py
@@ -7,18 +7,17 @@ def form_grid(grid):
 | {grid[6]} | {grid[7]} | {grid[8]} |
 ''')
 
-
+# Setting the winning combinations
 win_comb = [ (0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6) ]
 
 def winner(grid1, player):
-    for win in win_comb:
+    for win in win_comb: #To verify winning combination
         if grid1[win[0]] == grid1[win[1]] == grid1[win[2]] == player:
             return True
     return False
 
-
 def play_game():
-    grid1 = [" " for _ in range(9)]
+    grid1 = [" "]*9
     player = "X"
     game_over = False
 
@@ -45,13 +44,13 @@ def play_game():
                 else:
                     print("Spot taken or out of range. Enter a valid spot")
             except ValueError:
-                print("Please enter a valid number (1-9).")
+                print("Please enter a valid number (1-9).")  #Exception Handling
 
         if winner(grid1, player):
             form_grid(grid1)
             print(f"Player {player} wins!")
             game_over = True
-        elif " " not in grid1:
+        elif " " not in grid1: #Checking for draw
             form_grid(grid1)
             print("It's a draw!")
             game_over = True
